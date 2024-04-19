78822aa5fab81f6bb235761a91562fd23b6044cc
BuildTools <unconfigured@null.spigotmc.org>
2024-04-19 22:15:01 +0100

    test db binding

--- frontend/functions/api/helloworld.js
+++ frontend/functions/api/helloworld.js
@@ -1,3 +1,10 @@
 export function onRequest(context) {
-    return new Response("Hello, world!")
+    //return new Response("Hello, world!")
+    // If you did not use `DB` as your binding name, change it here
+    const { results } = await env.DB.prepare(
+        "SELECT * FROM Customers WHERE CompanyName = ?"
+    )
+        .bind("Bs Beverages")
+        .all();
+    return Response.json(results);
 }
