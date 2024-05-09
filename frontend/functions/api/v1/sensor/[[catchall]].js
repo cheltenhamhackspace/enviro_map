96d910fa85d04bdadf0838f8f9bf9291e5c1e66d
BuildTools <unconfigured@null.spigotmc.org>
2024-05-09 23:48:12 +0100

    test

--- frontend/functions/api/v1/sensor/[[catchall]].js
+++ frontend/functions/api/v1/sensor/[[catchall]].js
@@ -2,11 +2,11 @@ export async function onRequest(context) {
 
     if (context.params.catchall.length === 2 && context.params.catchall[1] === "latest") {
         const latestReading = await context.env.READINGS_TABLE.prepare(
-            "SELECT device_id, pm2_5, nox, voc, MAX(event_time) FROM sensor_readings WHERE device_id = ?"
+            "SELECT pm2_5, nox, voc, MAX(event_time) FROM sensor_readings WHERE device_id = ?"
         ).bind(context.params.catchall[0]).all();
         console.log(latestReading);
 
-        if (latestReading.results.length == 1) {
+        if (latestReading.results.length == 1 && latestReading.results[0]["MAX(event_time)"] !== null) {
             return Response.json(latestReading.results[0]);
         }
         else {
