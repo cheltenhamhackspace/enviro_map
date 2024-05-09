465ab9a2b537520c8d9b3f8a402fde2aace4d836
BuildTools <unconfigured@null.spigotmc.org>
2024-05-09 20:05:35 +0100

    test

--- frontend/functions/api/v1/sensors.js
+++ frontend/functions/api/v1/sensors.js
@@ -1,6 +1,17 @@
 export async function onRequest(context) {
-    const { results } = await context.env.READINGS_TABLE.prepare(
+    const allSensors = await context.env.READINGS_TABLE.prepare(
         "SELECT name, device_id, lat, long FROM sensors"
     ).all();
+
+    const latestReadings = await context.env.READINGS_TABLE.prepare(
+        "SELECT device_id, voc, MAX(event_time) FROM sensor_readings GROUP BY device_id"
+    ).all();
+
+    latestReadings.forEach(function (arrayItem) {
+        let index = allSensors.findIndex(x => x.device_id === arrayItem.device_id);
+        console.log(index);
+        allSensors[index].pm2_5 = arrayItem.pm2_5;
+    });
+
     return Response.json(results);
 }
