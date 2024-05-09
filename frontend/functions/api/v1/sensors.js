bcf8add227297100ed085e13e3503bf5881257c1
BuildTools <unconfigured@null.spigotmc.org>
2024-05-09 23:31:21 +0100

    test

--- frontend/functions/api/v1/sensors.js
+++ frontend/functions/api/v1/sensors.js
@@ -3,23 +3,25 @@ export async function onRequest(context) {
         "SELECT name, device_id, lat, long FROM sensors"
     ).all();
 
-    const latestReadings = await context.env.READINGS_TABLE.prepare(
-        "SELECT device_id, pm2_5, nox, voc, MAX(event_time) FROM sensor_readings GROUP BY device_id"
-    ).all();
+    const allSensorIds = allSensors.results.map(a => a.device_id);
+    console.log(allSensorIds);
+
+    const dbQueryLatestReadings = await context.env.READINGS_TABLE.prepare(
+        "SELECT device_id, pm2_5, nox, voc, MAX(event_time) FROM sensor_readings WHERE device_id = ?"
+    )
 
     console.log(allSensors);
-    console.log(latestReadings);
 
 
 
-    latestReadings.results.forEach(function (arrayItem) {
-        let index = allSensors.results.findIndex(x => x.device_id === arrayItem.device_id);
-        console.log(index);
-        if (index >= 0) {
+    latestReadings.results.forEach(async function (arrayItem) {
+        const latestReading = await dbQueryLatestReadings.bind(arrayItem.device_id).all();
+        console.log(latestReading);
+        if (latestReading.results.length == 1) {
             console.log(arrayItem);
-            allSensors.results[index]["pm2_5"] = arrayItem.pm2_5;
-            allSensors.results[index]["nox"] = arrayItem.nox;
-            allSensors.results[index]["voc"] = arrayItem.voc;
+            allSensors.results[index]["pm2_5"] = latestReading.results[0].pm2_5;
+            allSensors.results[index]["nox"] = latestReading.results[0].nox;
+            allSensors.results[index]["voc"] = latestReading.results[0].voc;
         }
     });
 
