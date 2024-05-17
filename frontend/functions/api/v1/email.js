98e72b3c5a5570023a858609203e64eb5bcbc024
BuildTools <unconfigured@null.spigotmc.org>
2024-05-17 21:08:12 +0100

    maybe make email code work this time

--- frontend/functions/api/v1/email.js
+++ frontend/functions/api/v1/email.js
@@ -1,35 +1,33 @@
-export default {
-    async fetch() {
-        // For testing purpose, replace this with your personal email
-        // so that you can see the message sent to your inbox
-        const receiver = context.env.owner_email;
-        // Replace <yourcompany.com> with the domain you set up earlier
-        const sender = 'map@cheltenham.space'
-        const send_request = new Request('https://api.mailchannels.net/tx/v1/send', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json',
-            },
-            body: JSON.stringify({
-                personalizations: [
-                    {
-                        to: [{ email: receiver, name: 'Test Recipient' }],
-                    },
-                ],
-                from: {
-                    email: sender,
-                    name: 'Cloudflare Workers - MailChannels integration',
+export async function onRequest(context) {
+    // For testing purpose, replace this with your personal email
+    // so that you can see the message sent to your inbox
+    const receiver = context.env.owner_email;
+    // Replace <yourcompany.com> with the domain you set up earlier
+    const sender = 'map@cheltenham.space'
+    const send_request = new Request('https://api.mailchannels.net/tx/v1/send', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+            personalizations: [
+                {
+                    to: [{ email: receiver, name: 'Test Recipient' }],
                 },
-                subject: 'Look! No servers',
-                content: [
-                    {
-                        type: 'text/html',
-                        value: '<h1>Hello from Cloudflare worker</h1>',
-                    },
-                ],
-            }),
-        })
-        const resp = await fetch(send_request)
-        return new Response(await resp.ok)
-    },
+            ],
+            from: {
+                email: sender,
+                name: 'Cloudflare Workers - MailChannels integration',
+            },
+            subject: 'Look! No servers',
+            content: [
+                {
+                    type: 'text/html',
+                    value: '<h1>Hello from Cloudflare worker</h1>',
+                },
+            ],
+        }),
+    })
+    const resp = await fetch(send_request)
+    return new Response(await resp.ok)
 }
