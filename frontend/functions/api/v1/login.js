a395b959b808cd23844513b690c9e785120ca458
BuildTools <unconfigured@null.spigotmc.org>
2024-05-19 17:58:23 +0100

    try and format email a little better

--- frontend/functions/api/v1/login.js
+++ frontend/functions/api/v1/login.js
@@ -76,7 +76,7 @@ export async function onRequest(context) {
                 content: [
                     {
                         type: 'text/html',
-                        value: `<h1>Hello from Cheltenham hackspace enviro map</h1>\nThis function is a work in progress at the moment... sorry.\nTemp JWT: ${jwt}\n:)`,
+                        value: `<h1>Hello from Cheltenham hackspace enviro map</h1><p>This function is a work in progress at the moment... sorry.<br />Temp JWT: ${jwt}</p><p>:</p>)`,
                     },
                 ],
             }),
