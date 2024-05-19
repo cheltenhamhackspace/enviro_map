163da8d421be51d7a433c508f7ecbf1de6367de1
BuildTools <unconfigured@null.spigotmc.org>
2024-05-19 18:40:44 +0100

    test

--- frontend/functions/api/v1/verify.js
+++ frontend/functions/api/v1/verify.js
@@ -8,29 +8,23 @@ export async function onRequest(context) {
     let jwt = urlParams.get("jwt");
 
     if (jwt) {
+
+        let verifyResults;
+
         const alg = 'RS256';
         const publicKey = await jose.importSPKI(context.env.JWT_PUBLIC_KEY, alg);
 
-        let { payload, protectedHeader } = await jose.jwtVerify(jwt, publicKey, {
-            issuer: 'testIssuer',
-            audience: 'testAudience',
-        });
-
         try {
-            console.log("payload", payload);
+            verifyResults = await jose.jwtVerify(jwt, publicKey, {
+                issuer: 'testIssuer',
+                audience: 'testAudience',
+            });
         }
         catch (error) {
-            console.log(error);
+            Response(`JWT validation error: ${error}`, { status: 500 })
         }
 
-        try {
-            console.log("protectedHeader", protectedHeader);
-        }
-        catch (error) {
-            console.log(error);
-        }
-
-        return new Response(`JWT verified ${JSON.stringify(payload)}`, {
+        return new Response(`JWT verified ${JSON.stringify(verifyResults)}`, {
             headers: {
                 "content-type": "text/html;charset=UTF-8",
             }
