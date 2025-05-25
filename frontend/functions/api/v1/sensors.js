a9da528502432890632b197217f678db24dde3e7
Rory B <unconfigured@null>
2025-05-25 23:30:28 +0100

    update sensor listing and login code

--- frontend/functions/api/v1/sensors.js
+++ frontend/functions/api/v1/sensors.js
@@ -1,29 +1,47 @@
+/**
+ * Sensors API Endpoint
+ * Returns a list of all sensors with their basic information
+ */
 export async function onRequest(context) {
-    const allSensors = await context.env.READINGS_TABLE.prepare(
-        "SELECT name, device_id, lat, long FROM sensors"
-    ).all();
+    try {
+        // Query all sensors from the database
+        const allSensors = await context.env.READINGS_TABLE.prepare(
+            "SELECT name, device_id, lat, long FROM sensors"
+        ).all();
 
-    // const allSensorIds = allSensors.results.map(a => a.device_id);
-    // console.log(allSensorIds);
+        // Return the sensor data as JSON
+        return new Response(JSON.stringify(allSensors.results), {
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Cache-Control': 'public, max-age=300' // Cache for 5 minutes
+            }
+        });
+    } catch (error) {
+        console.error('Error fetching sensors:', error);
+        
+        return new Response(JSON.stringify({
+            error: 'Failed to fetch sensors',
+            message: error.message
+        }), {
+            status: 500,
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
+        });
+    }
+}
 
-    // const dbQueryLatestReadings = await context.env.READINGS_TABLE.prepare(
-    //     "SELECT device_id, pm2_5, nox, voc, MAX(event_time) FROM sensor_readings WHERE device_id = ?"
-    // )
-
-    // console.log(allSensors);
-
-
-
-    // allSensors.results.forEach(async function (arrayItem) {
-    //     const latestReading = await dbQueryLatestReadings.bind(arrayItem.device_id).all();
-    //     console.log(latestReading);
-    //     if (latestReading.results.length == 1) {
-    //         console.log(arrayItem);
-    //         allSensors.results[index]["pm2_5"] = latestReading.results[0].pm2_5;
-    //         allSensors.results[index]["nox"] = latestReading.results[0].nox;
-    //         allSensors.results[index]["voc"] = latestReading.results[0].voc;
-    //     }
-    // });
-
-    return Response.json(allSensors.results);
+// Handle OPTIONS requests for CORS preflight
+export async function onRequestOptions() {
+    return new Response(null, {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type',
+        }
+    });
 }
